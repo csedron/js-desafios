dc63bfb0e8f1521112c5ae482861a34a000673c9
Claudio Sedron <claudiosedron@gmail.com>
2021-01-21 16:16:04 -0300

    desafio 4

---
+++ desafio4.js
@@ -0,0 +1,7 @@
+var carroCompras = 0;
+
+function Sumar(prod1,prod2,prod3){
+    var resultado = prod1 + prod2 + prod3;
+    carroCompras = carroCompras + resultado;
+    console.log(carroCompras);
+}
