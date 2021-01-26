035ddadbc7019c81c07fa49fcaefe6833e271326
Claudio Sedron <claudiosedron@gmail.com>
2021-01-26 18:33:38 -0300

    desafio clase 5

---
+++ desafio5.js
@@ -0,0 +1,19 @@
+var carroCompras = 0;
+
+function Sumar(prod1, prod2, prod3) {
+  var resultado = prod1 + prod2 + prod3;
+  carroCompras = carroCompras + resultado;
+  console.log(carroCompras);
+}
+
+class Producto {
+  constructor(nombreMarca, precioArticulo, cantidadProducto) {
+    this.nombre = nombreMarca;
+    this.precio = precioArticulo;
+    this.stock = cantidadProducto;
+  }
+}
+let productoUno = new Producto("Pendrive 64 Gb.", 1500, 15);
+let productoDos = new Producto("reparacion MAC", 2000, 1000);
+let productoTres = new Producto("Radeon RX 580", 18000, 12);
+console.log(productoDos,productoDos,productoTres);
